ad54c7090dd1f6da13e8771f438a06255a2b618a
Antonio Fabregat <fabregat.antonio@gmail.com>
2014-12-02 18:23:11 +0000

    reliable pathways strategy improved

--- src/main/java/uk/ac/ebi/models2pathways/tools/reactome/AnalysisServiceHandler.java
+++ src/main/java/uk/ac/ebi/models2pathways/tools/reactome/AnalysisServiceHandler.java
@@ -9,9 +9,8 @@ import uk.ac.ebi.models2pathways.resources.mapping.reactomeanalysisservice.Resou
 import uk.ac.ebi.models2pathways.tools.sbml.SBMLModel;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author Maximilian Koch <mkoch@ebi.ac.uk>
@@ -29,45 +28,36 @@ public class AnalysisServiceHandler {
             }
             AnalysisResult analysisResult = AnalysisResultConverter.getAnalysisResultObject(jsonResult);
             if (analysisResult.getPathwaysFound() != 0) {
-                if (tokenRequestRequired(analysisResult.getResourceSummary())) {
-                    String token = analysisResult.getSummary().getToken();
-                    String resourceSummary = getResource(analysisResult.getResourceSummary());
-                    httpResponse = AnalysisServiceRequest.requestByToken(token, resourceSummary);
-                    try {
-                        HttpEntity entity = httpResponse.getEntity();
-                        jsonResult = EntityUtils.toString(entity, "UTF-8");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    analysisResult = AnalysisResultConverter.getAnalysisResultObject(jsonResult);
+                String token = analysisResult.getSummary().getToken();
+                String resourceSummary = getResource(analysisResult.getResourceSummary());
+                httpResponse = AnalysisServiceRequest.requestByToken(token, resourceSummary);
+                try {
+                    HttpEntity entity = httpResponse.getEntity();
+                    jsonResult = EntityUtils.toString(entity, "UTF-8");
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+                analysisResult = AnalysisResultConverter.getAnalysisResultObject(jsonResult);
             }
-            if (onlyNecessaryPathways(analysisResult.getPathways())) {
-                return analysisResult;
-            }
+
+            analysisResult.setPathways(getReliablePathways(analysisResult.getPathways()));
+            return analysisResult;
         }
         return null;
     }
 
-    private static boolean onlyNecessaryPathways(List<PathwaySummary> pathways) {
-        Set<PathwaySummary> unnecessaryPathways = new HashSet<PathwaySummary>();
+    private static List<PathwaySummary> getReliablePathways(List<PathwaySummary> pathways) {
+        List<PathwaySummary> reliablePathways = new ArrayList<PathwaySummary>();
         for (PathwaySummary pathway : pathways) {
-            if (!pathway.isLlp()) {
-                unnecessaryPathways.add(pathway);
-            }
-            if (pathway.getEntities().getFdr() >= 0.005) {
-                unnecessaryPathways.add(pathway);
+            if (pathway.isLlp() && pathway.getEntities().getpValue() <= 0.005) {
+                reliablePathways.add(pathway);
             }
         }
-        pathways.removeAll(unnecessaryPathways);
-        return true;
-    }
-
-    private static boolean tokenRequestRequired(List<ResourceSummary> resourceSummary) {
-        return resourceSummary.size() < 3;
+        return reliablePathways;
     }
 
     private static String getResource(List<ResourceSummary> resourceSummary) {
-        return resourceSummary.get(1).getResource();
+        ResourceSummary rs = resourceSummary.size() == 2 ? resourceSummary.get(1) : resourceSummary.get(0);
+        return rs.getResource();
     }
 }
