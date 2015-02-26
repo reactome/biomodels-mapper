e5cf5e9ccdba6dc444ea0e0d29d472a1c31f87c4
maximiliankoch <tschaka1904@gmail.com>
2015-02-26 09:52:27 +0000

    A simple file writer class to produce the needed tsv file.

---
+++ core/src/main/java/org/reactome/server/core/output/FileHandler.java
@@ -0,0 +1,87 @@
+package org.reactome.server.core.output;
+
+import org.reactome.server.core.model.reactome.AnalysisResult;
+import org.reactome.server.core.model.reactome.PathwaySummary;
+import org.reactome.server.core.model.sbml.SBMLModel;
+import org.reactome.server.core.utils.URLBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Maximilian Koch (mkoch@ebi.ac.uk).
+ */
+public class FileHandler {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("YYYY-MM-dd_hh-mm-ss");
+    private static final String FILE_NAME = "models2pathways";
+    private static String locationPath;
+    private static FileWriter fileWriter;
+
+
+    public static void createFile() {
+        try {
+            fileWriter = new FileWriter(getLocationPath() + ".tsv", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addHeader() {
+        try {
+            fileWriter.write("Species\t" +
+                    "BioModel\t" +
+                    "Pathway\t" +
+                    "Analysis Link\t" +
+                    "Total entities (BioModel)\t" +
+                    "Total entities (Pathway)\t" +
+                    "Matched BioModel entities in Pathway\t" +
+                    "Overlap in %\t" +
+                    "pValue\t" +
+                    "FDR\t" +
+                    "isValid\t" +
+                    "Reactome Curator\t" +
+                    "BioModels Curator\t" +
+                    "Comments\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addRow(PathwaySummary pathwaySummary, AnalysisResult analysisResult, SBMLModel sbmlModel, boolean hasMinPValue) {
+        try {
+            //TODO urls to properties
+            fileWriter.write(pathwaySummary.getSpecies().getName() + "\t" +
+                    "=HYPERLINK(\"www.ebi.ac.uk/biomodels-main/" + sbmlModel.getBioModelsID() + "\", \"" + sbmlModel.getName() + "\")" + "\t" +
+                    "=HYPERLINK(\"www.reactome.org/content/detail/" + pathwaySummary.getStId() + "\", \"" + pathwaySummary.getName() + "\")" + "\t" +
+                    "=HYPERLINK(\"" + URLBuilder.getAnalysisURL(pathwaySummary.getSpecies().getDbId(), analysisResult.getToken()) + "\", \" Analysis Link\")" + "\t" +
+                    sbmlModel.getSBMLModelAnnotations().size() + "\t" +
+                    pathwaySummary.getEntities().getTotal() + "\t" +
+                    pathwaySummary.getEntities().getFound() + "\t" +
+                    (double) pathwaySummary.getEntities().getFound() / Math.max((double) sbmlModel.getSBMLModelAnnotations().size(), (double) pathwaySummary.getEntities().getTotal()) + "\t" +
+                    pathwaySummary.getEntities().getpValue() + "\t" +
+                    pathwaySummary.getEntities().getFdr() + "\t" +
+                    hasMinPValue + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeFile() {
+        try {
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getLocationPath() {
+        return locationPath + FILE_NAME + DATE_FORMAT.format(new Date());
+    }
+
+    public static void setLocationPath(String locationPath) {
+        FileHandler.locationPath = locationPath;
+    }
+}
