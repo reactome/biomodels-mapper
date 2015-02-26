2d64b3165ca6bc297d80c1973f8209ab8cbe2b03
maximiliankoch <tschaka1904@gmail.com>
2015-02-26 10:41:45 +0000

    Added token and species filter

--- core/src/main/java/org/reactome/server/core/helper/AnalysisServiceHandler.java
+++ core/src/main/java/org/reactome/server/core/helper/AnalysisServiceHandler.java
@@ -6,10 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
-import org.reactome.server.core.model.sbml.SBMLModel;
+import org.reactome.server.core.enums.Species;
 import org.reactome.server.core.model.reactome.AnalysisResult;
 import org.reactome.server.core.model.reactome.PathwaySummary;
 import org.reactome.server.core.model.reactome.ResourceSummary;
+import org.reactome.server.core.model.sbml.SBMLModel;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ public class AnalysisServiceHandler {
 
     public static AnalysisResult getReactomeAnalysisResultBySBMLModel(SBMLModel sbmlModel, Double customPValue) {
         HttpResponse httpResponse = AnalysisServiceRequest.requestByModel(sbmlModel);
+        String token = null;
         String jsonResult = null;
         if (httpResponse.getStatusLine().getStatusCode() == 200) {
             try {
@@ -33,7 +35,7 @@ public class AnalysisServiceHandler {
             }
             AnalysisResult analysisResult = getAnalysisResultObject(jsonResult);
             if (analysisResult.getPathwaysFound() != 0) {
-                String token = analysisResult.getSummary().getToken();
+                token = analysisResult.getSummary().getToken();
                 String resourceSummary = getResource(analysisResult.getResourceSummary());
                 httpResponse = AnalysisServiceRequest.requestByToken(token, resourceSummary);
                 try {
@@ -45,28 +47,35 @@ public class AnalysisServiceHandler {
                 analysisResult = getAnalysisResultObject(jsonResult);
             }
 
-            analysisResult.setPathways(getReliablePathways(analysisResult.getPathways(), customPValue));
+            analysisResult.setPathways(getReliablePathways(analysisResult.getPathways(), customPValue, sbmlModel));
+            analysisResult.setToken(token);
             return analysisResult;
         }
         return null;
     }
 
-    private static List<PathwaySummary> getReliablePathways(List<PathwaySummary> pathways, Double customPValue) {
+    private static List<PathwaySummary> getReliablePathways(List<PathwaySummary> pathways, Double customPValue, SBMLModel sbmlModel) {
         List<PathwaySummary> reliablePathways = new ArrayList<PathwaySummary>();
         for (PathwaySummary pathway : pathways) {
             if (pathway.isLlp() && pathway.getEntities().getpValue() <= customPValue) {
-                reliablePathways.add(pathway);
+                try {
+                    if (pathway.getSpecies().getDbId().toString().equals(Species.getSpeciesByBioModelsTaxonomyid(sbmlModel.getBioModelsTaxonomyId().getBioModelsTaxonomyId()).getReactomeTaxonomyId()))
+                        reliablePathways.add(pathway);
+                } catch (NullPointerException e) {
+                    continue;
+                }
+
             }
         }
         return reliablePathways;
     }
 
-    private static String getResource(List<ResourceSummary> resourceSummary) {
+    public static String getResource(List<ResourceSummary> resourceSummary) {
         ResourceSummary rs = resourceSummary.size() == 2 ? resourceSummary.get(1) : resourceSummary.get(0);
         return rs.getResource();
     }
 
-    public static AnalysisResult getAnalysisResultObject(String jsonString) {
+    private static AnalysisResult getAnalysisResultObject(String jsonString) {
         AnalysisResult analysisResult = null;
         ObjectMapper mapper = new ObjectMapper();
 
