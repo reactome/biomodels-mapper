9841e383487cebb1f75c3e0edbb2a31c742ce71d
maximiliankoch <tschaka1904@gmail.com>
2015-03-10 15:46:28 +0000

    clean up and adoption for reliable pathways

--- core/src/main/java/org/reactome/server/core/helper/Consumer.java
+++ core/src/main/java/org/reactome/server/core/helper/Consumer.java
@@ -1,5 +1,6 @@
 package org.reactome.server.core.helper;
 
+import org.reactome.server.core.database.DataSourceFactory;
 import org.reactome.server.core.database.DatabaseInsertionHelper;
 import org.reactome.server.core.entrypoint.Models2Pathways;
 import org.reactome.server.core.model.reactome.AnalysisResult;
@@ -16,17 +17,19 @@ import java.util.logging.Logger;
 public class Consumer implements Runnable {
     final static Logger logger = Logger.getLogger(Consumer.class.getName());
 
-    private Double significantPValue;
-    private Double extendedPValue;
+    private Double significantFDR;
+    private Double extendedFDR;
+    private Double reactionCoverage;
     private BlockingQueue<SBMLModel> sbmlModelBlockingQueue;
     private Integer numberOfCreatePathwayEntries = 0;
     private Integer numberOfCreateBioModelEntries = 0;
     private Integer numberOfCreateXReferences = 0;
 
-    public Consumer(BlockingQueue<SBMLModel> sbmlModelBlockingQueue, String significantPValue, String extendedPValue) {
+    public Consumer(BlockingQueue<SBMLModel> sbmlModelBlockingQueue, String significantFDR, String extendedFDR, String reactionCoverage) {
         this.sbmlModelBlockingQueue = sbmlModelBlockingQueue;
-        this.significantPValue = Double.valueOf(significantPValue);
-        this.extendedPValue = Double.valueOf(extendedPValue);
+        this.significantFDR = Double.valueOf(significantFDR);
+        this.extendedFDR = Double.valueOf(extendedFDR);
+        this.reactionCoverage = Double.valueOf(reactionCoverage);
     }
 
     @Override
@@ -38,7 +41,7 @@ public class Consumer implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            boolean hasMinPValue = true;
+            boolean hasMinFDR = true;
 
 
             //Remove all trivial chemicals
@@ -46,26 +49,26 @@ public class Consumer implements Runnable {
                 sbmlModel.getSBMLModelAnnotations().removeAll(new TrivialChemicals().getTrivialChemicals());
             }
 
-            AnalysisResult analysisResult = AnalysisServiceHandler.getReactomeAnalysisResultBySBMLModel(sbmlModel, significantPValue);
+            AnalysisResult analysisResult = AnalysisServiceHandler.getReactomeAnalysisResultBySBMLModel(sbmlModel, significantFDR, reactionCoverage);
             if (analysisResult == null) {
                 int tries = 2;
                 while (analysisResult == null && tries != 0) {
-                    analysisResult = AnalysisServiceHandler.getReactomeAnalysisResultBySBMLModel(sbmlModel, significantPValue);
+                    analysisResult = AnalysisServiceHandler.getReactomeAnalysisResultBySBMLModel(sbmlModel, significantFDR, reactionCoverage);
                     tries = tries - 1;
                     if (analysisResult == null && tries == 0) {
                         logger.info(sbmlModel + " Couldn't request analysis result");
                         logger.info(sbmlModel + " retry to get analysis result");
                     }
                 }
-            } else if (analysisResult.getPathwaysFound() == 0) {
-                logger.info("Couldn't find pathways on pValue " + significantPValue);
-                logger.info("Request on extended pValue " + extendedPValue);
-                hasMinPValue = false;
-                analysisResult = AnalysisServiceHandler.getReactomeAnalysisResultBySBMLModel(sbmlModel, extendedPValue);
+            } else if (analysisResult.getPathways().size() != 0 && analysisResult.getReliablePathways().size() == 0) {
+                logger.info("Couldn't find pathways on FDR " + significantFDR);
+                logger.info("Request on extended FDR " + extendedFDR);
+                hasMinFDR = false;
+                analysisResult = AnalysisServiceHandler.getReactomeAnalysisResultBySBMLModel(sbmlModel, extendedFDR, reactionCoverage);
                 if (analysisResult == null) {
                     int tries = 2;
                     while (analysisResult == null && tries != 0) {
-                        analysisResult = AnalysisServiceHandler.getReactomeAnalysisResultBySBMLModel(sbmlModel, extendedPValue);
+                        analysisResult = AnalysisServiceHandler.getReactomeAnalysisResultBySBMLModel(sbmlModel, extendedFDR, reactionCoverage);
                         tries = tries - 1;
                         if (analysisResult == null && tries == 0) {
                             logger.info(" >> Couldn't request analysis result");
@@ -73,24 +76,27 @@ public class Consumer implements Runnable {
                     }
                 }
                 if (analysisResult == null || analysisResult.getPathwaysFound() == 0) {
-                    logger.info("No pathways found with pValue " + extendedPValue + " on " + sbmlModel.getBioModelsID());
+                    logger.info("No pathways found with FDR " + extendedFDR + " on " + sbmlModel.getBioModelsID());
                 }
             }
             if (analysisResult != null) {
-                //Database
-                DatabaseInsertionHelper.createNewBioModelEntry(sbmlModel);
-                numberOfCreateBioModelEntries += 1;
-                for (PathwaySummary pathwaySummary : analysisResult.getPathways()) {
-                    //Database
-                    DatabaseInsertionHelper.createNewPathwayEntry(pathwaySummary);
-                    numberOfCreatePathwayEntries += 1;
-                    DatabaseInsertionHelper.createNewXReferenceEntry(pathwaySummary, sbmlModel, hasMinPValue, false);
-                    numberOfCreateXReferences += 1;
-                    //File output
-                    if (FileHandler.isInitialized) {
-                        FileHandler.addRow(pathwaySummary, analysisResult, sbmlModel, hasMinPValue);
+                //TODO needs to be changed
+                if (DataSourceFactory.isHasBeenInitialized()) {
+                    DatabaseInsertionHelper.createNewBioModelEntry(sbmlModel);
+                    numberOfCreateBioModelEntries += 1;
+                    for (PathwaySummary pathwaySummary : analysisResult.getReliablePathways()) {
+                        //Database
+                        DatabaseInsertionHelper.createNewPathwayEntry(pathwaySummary);
+                        numberOfCreatePathwayEntries += 1;
+                        DatabaseInsertionHelper.createNewXReferenceEntry(pathwaySummary, sbmlModel, hasMinFDR, false);
+                        numberOfCreateXReferences += 1;
+                        //File output
+                        if (FileHandler.isInitialized) {
+                            FileHandler.addRow(pathwaySummary, analysisResult, sbmlModel, hasMinFDR);
+                        }
                     }
                 }
+
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
