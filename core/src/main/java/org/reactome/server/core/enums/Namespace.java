b0d333e364befec193c809203ef77e4d94092bf1
maximiliankoch <tschaka1904@gmail.com>
2015-03-13 17:36:14 +0000

    clean up

--- core/src/main/java/org/reactome/server/core/enums/Namespace.java
+++ core/src/main/java/org/reactome/server/core/enums/Namespace.java
@@ -27,30 +27,3 @@ public enum Namespace {
                 '}';
     }
 }
-//    UNIPROT("uniprot", new String[]{"uniprot"}),
-//        CHEBI("chebi", new String[]{"chebi", "obo.chebi"}),
-//        CHEMBL_COMPOUND("chembl.compound", new String[]{"compound.compound"}),
-//        ENSEMBL("uniprot", new String[]{"ensembl"});
-//
-//private final String namespace;
-//private final String [] namespaceAlternatives;
-//
-//        Namespace(String namespace, String[] namespaceAlternatives) {
-//        this.namespace = namespace;
-//        this.namespaceAlternatives = namespaceAlternatives;
-//        }
-//
-//public String getNamespace() {
-//        return namespace;
-//        }
-//
-//public String[] getNamespaceAlternatives() {
-//        return namespaceAlternatives;
-//        }
-//
-//@Override
-//public String toString() {
-//        return "Namespaces{" +
-//        "namespace='" + namespace + '\'' +
-//        '}';
-//        }
