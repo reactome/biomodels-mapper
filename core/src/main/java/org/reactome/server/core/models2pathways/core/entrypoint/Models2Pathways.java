0330488cd69aa2a0cb3b9fc99a5d545dbfcc4bb5
maximiliankoch <tschaka1904@gmail.com>
2015-03-20 15:41:53 +0000

    some smaller changes. not ready yet!!

--- core/src/main/java/org/reactome/server/core/models2pathways/core/entrypoint/Models2Pathways.java
+++ core/src/main/java/org/reactome/server/core/models2pathways/core/entrypoint/Models2Pathways.java
@@ -1,12 +1,17 @@
-package org.reactome.server.core.entrypoint;
+package org.reactome.server.core.models2pathways.core.entrypoint;
 
 import com.martiansoftware.jsap.JSAPResult;
-import org.reactome.server.core.helper.Consumer;
-import org.reactome.server.core.helper.Producer;
-import org.reactome.server.core.model.sbml.SBMLModel;
-import org.reactome.server.core.output.FileExporter;
-import org.reactome.server.core.utils.JSAPHandler;
+import org.reactome.server.core.models2pathways.biomodels.model.SBMLModel;
+import org.reactome.server.core.models2pathways.core.Consumer;
+import org.reactome.server.core.models2pathways.core.Producer;
+import org.reactome.server.core.models2pathways.core.helper.NameSpaceHelper;
+import org.reactome.server.core.models2pathways.core.helper.SpeciesHelper;
+import org.reactome.server.core.models2pathways.core.helper.TrivialChemicalHelper;
+import org.reactome.server.core.models2pathways.core.utils.FileExporter;
+import org.reactome.server.core.models2pathways.core.utils.JSAPHandler;
+import org.reactome.server.core.models2pathways.core.utils.ProptertiesLoader;
 
+import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.logging.Logger;
@@ -18,7 +23,7 @@ import java.util.logging.Logger;
 public class Models2Pathways {
     final static Logger logger = Logger.getLogger(Models2Pathways.class.getName());
 
-    final static int BLOCKING_QUEUE_SIZE = 5;
+    final static int BLOCKING_QUEUE_SIZE = 3;
 
     private static Thread PRODUCER;
 
@@ -28,11 +33,17 @@ public class Models2Pathways {
         JSAPResult jsapResult = JSAPHandler.ArgumentHandler(args);
         FileExporter.setLocationPath(jsapResult.getString("output"));
 
+        try {
+            SpeciesHelper.getInstance().setSpecies(ProptertiesLoader.readSpecies(""));
+            NameSpaceHelper.getInstance().setNamespaces(ProptertiesLoader.readNamespaces(""));
+            TrivialChemicalHelper.getInstance().setTrivialChemicals(ProptertiesLoader.readTrivialChemicals(""));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
         //Setting up output file
         FileExporter.createFile();
-        if (jsapResult.getBoolean("header")) {
-            FileExporter.addHeader();
-        }
 
         //Shared blockingqueue for producert consumer.
         BlockingQueue<SBMLModel> sbmlModelBlockingQueue = new LinkedBlockingDeque<>(BLOCKING_QUEUE_SIZE);
