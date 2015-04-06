6e5eca891c68be696c1ddf6224eb418cb3987144
maximiliankoch <tschaka1904@gmail.com>
2015-04-07 00:09:03 +0100

    PropertiesLoad. Maybe needs to be a bit refactoring.

--- core/src/main/java/org/reactome/server/core/models2pathways/core/utils/PropertiesLoader.java
+++ core/src/main/java/org/reactome/server/core/models2pathways/core/utils/PropertiesLoader.java
@@ -5,9 +5,9 @@ import org.reactome.server.core.models2pathways.core.model.Specie;
 import org.reactome.server.core.models2pathways.core.model.TrivialChemical;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,47 +15,44 @@ import java.util.Set;
 /**
  * Created by Maximilian Koch (mkoch@ebi.ac.uk).
  */
-public class ProptertiesLoader {
+public class PropertiesLoader {
     private static final String SEPARATOR = "\t";
 
-    public static Set<Specie> readSpecies(String inputPath) throws IOException {
+    private static BufferedReader readFile(InputStream inputStream) {
+        return new BufferedReader(new InputStreamReader(inputStream));
+    }
+
+    public Set<Specie> getSpecies() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("species.properties");
         Set<Specie> species = new HashSet<>();
         String line;
-        while ((line = readFile(inputPath).readLine()) != null) {
+        while ((line = readFile(inputStream).readLine()) != null) {
             String[] content = line.split(SEPARATOR);
+            System.out.println(content.length);
             species.add(new Specie(Long.valueOf(content[0]), Long.valueOf(content[1]), content[2]));
         }
         return species;
     }
 
-    public static Set<Namespace> readNamespaces(String inputPath) throws IOException {
+    public Set<Namespace> getNamespaces() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("namespaces.properties");
         Set<Namespace> namespaces = new HashSet<>();
         String line;
-        while ((line = readFile(inputPath).readLine()) != null) {
+        while ((line = readFile(inputStream).readLine()) != null) {
             String[] content = line.split(SEPARATOR);
             namespaces.add(new Namespace(content[0], Boolean.valueOf(content[1])));
         }
         return namespaces;
     }
 
-    public static Set<TrivialChemical> readTrivialChemicals(String inputPath) throws IOException {
+    public Set<TrivialChemical> getTrivialChemicals() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("trivialchemicals.properties");
         Set<TrivialChemical> trivialChemicals = new HashSet<>();
         String line;
-        while ((line = readFile(inputPath).readLine()) != null) {
+        while ((line = readFile(inputStream).readLine()) != null) {
             String[] content = line.split(SEPARATOR);
             trivialChemicals.add(new TrivialChemical(Long.valueOf(content[0]), content[1]));
         }
         return trivialChemicals;
-    }
-    
-    private static BufferedReader readFile(String inputPath){
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader =  new BufferedReader(new FileReader(inputPath));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        return bufferedReader;
     }
 }
